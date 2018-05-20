46caada2f60850dc6c0d6030cf547c7cfa25e041
Derek Christensen <derek@heavyd.net>
2018-05-20 15:54:20 -0600

    Added deployment config

--- src/bells-music/src/App.tsx
+++ src/bells-music/src/App.tsx
@@ -4,11 +4,12 @@ import SongPage from './SongPage';
 import SongIndex from './SongIndex';
 import './App.css';
 
+const root = process.env.NODE_ENV === 'production' ? '/bells' : '.';
 class App extends React.Component {
   public render() {
     return (
       <div className="App">
-        <BrowserRouter>
+        <BrowserRouter basename={root}>
           <Switch>
             <Route path="/" exact={true} component={SongIndex} />
             <Route path="/song/:name" component={SongPage} />
