9ea5967cfd7ca668c1e1d8709f81243a33bc311d
Derek Christensen <derek@heavyd.net>
2018-11-27 23:54:28 -0700

    Fixing some songs

--- src/bells-music/src/songs/we_wish_you_a_merry_christmas.ts
+++ src/bells-music/src/songs/we_wish_you_a_merry_christmas.ts
@@ -1,1 +1,36 @@
-export default {"title":"We Wish You A Merry Christmas","url":"we-wish-you-a-merry-christmas","notes":[{"note":"C","octave":5,"lyric":"We"},{"note":"F","octave":4,"lyric":"wish"},{"note":"F","octave":4,"lyric":"you"},{"note":"G","octave":4,"lyric":"a"},{"note":"F","octave":4,"lyric":"mer-"},{"note":"E","octave":4,"lyric":"ry"},{"note":"D","octave":4,"lyric":"Christ-"},{"note":"D","octave":4,"lyric":"mas"},{"note":null,"octave":4,"lyric":"Break"},{"note":"D","octave":4,"lyric":"We"},{"note":"G","octave":4,"lyric":"wish"},{"note":"G","octave":4,"lyric":"you"},{"note":"A","octave":4,"lyric":"a"},{"note":"G","octave":4,"lyric":"mer-"},{"note":"F","octave":4,"lyric":"ry"},{"note":"E","octave":4,"lyric":"Christ-"},{"note":"C","octave":5,"lyric":"mas"},{"note":"C","octave":5,"lyric":"We"},{"note":"A","octave":4,"lyric":"wish"},{"note":"A","octave":4,"lyric":"you"},{"note":"Flat/Sharp","octave":4,"lyric":"a"},{"note":"A","octave":4,"lyric":"mer-"},{"note":"G","octave":4,"lyric":"ry"},{"note":"F","octave":4,"lyric":"Christ-"},{"note":"D","octave":4,"lyric":"mas"},{"note":"C","octave":5,"lyric":"And"},{"note":"C","octave":5,"lyric":"a"},{"note":"D","octave":4,"lyric":"hap-"},{"note":"G","octave":4,"lyric":"py"},{"note":"E","octave":4,"lyric":"new"},{"note":"F","octave":4,"lyric":"year"}]}
+export default {
+  title: 'We Wish You A Merry Christmas',
+  url: 'we-wish-you-a-merry-christmas',
+  notes: [
+    { note: 'C', octave: 5, lyric: 'We' },
+    { note: 'F', octave: 4, lyric: 'wish' },
+    { note: 'F', octave: 4, lyric: 'you' },
+    { note: 'G', octave: 4, lyric: 'a' },
+    { note: 'F', octave: 4, lyric: 'mer-' },
+    { note: 'E', octave: 4, lyric: 'ry' },
+    { note: 'D', octave: 4, lyric: 'Christ-' },
+    { note: 'D', octave: 4, lyric: 'mas' },
+    { note: 'D', octave: 4, lyric: 'We' },
+    { note: 'G', octave: 4, lyric: 'wish' },
+    { note: 'G', octave: 4, lyric: 'you' },
+    { note: 'A', octave: 4, lyric: 'a' },
+    { note: 'G', octave: 4, lyric: 'mer-' },
+    { note: 'F', octave: 4, lyric: 'ry' },
+    { note: 'E', octave: 4, lyric: 'Christ-' },
+    { note: 'C', octave: 5, lyric: 'mas' },
+    { note: 'C', octave: 5, lyric: 'We' },
+    { note: 'A', octave: 4, lyric: 'wish' },
+    { note: 'A', octave: 4, lyric: 'you' },
+    { note: 'B', incidental: 2, octave: 4, lyric: 'a' },
+    { note: 'A', octave: 4, lyric: 'mer-' },
+    { note: 'G', octave: 4, lyric: 'ry' },
+    { note: 'F', octave: 4, lyric: 'Christ-' },
+    { note: 'D', octave: 4, lyric: 'mas' },
+    { note: 'C', octave: 5, lyric: 'And' },
+    { note: 'C', octave: 5, lyric: 'a' },
+    { note: 'D', octave: 4, lyric: 'hap-' },
+    { note: 'G', octave: 4, lyric: 'py' },
+    { note: 'E', octave: 4, lyric: 'new' },
+    { note: 'F', octave: 4, lyric: 'year' }
+  ]
+};
