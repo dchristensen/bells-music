eeb42743caa17e19999f6570ccca71301307de74
Derek Christensen <derek@heavyd.net>
2021-12-14 00:30:58 -0700

    bunch of new songs

--- src/bells/app/data/songs.ts
+++ src/bells/app/data/songs.ts
@@ -1,17 +1,29 @@
 import Angels from "./songs/angels_we_have_heard_on_high";
+import AuldLangSyne from "./songs/auld_lang_syne";
 import Away from "./songs/away_in_a_manager";
 import ChristmasBells from "./songs/christmas_bells";
 import Deck from "./songs/deck_the_halls";
+import GoodKing from "./songs/good_king_wenceslas";
+import GoTellIt from "./songs/go_tell_it_on_the_mountain";
+import HappyBirthday from "./songs/happy_birthday";
+import Hark from "./songs/hark_the_herald_angels_sing";
+import IHeardTheBells from "./songs/i_heard_the_bells_on_christmas_day";
 import Wonder from "./songs/i_wonder_as_i_wander";
+import JingleBells from "./songs/jingle_bells";
 import Jolly from "./songs/jolly_old_saint_nicholas";
 import Joy from "./songs/joy_to_the_world";
 import Drummer from "./songs/little_drummer_boy";
+import OhCome from "./songs/oh_come_all_ye_faithful";
 import Sanctissima from "./songs/oh_sanctissima";
 import OverRiver from "./songs/over_the_river_and_through_the_woods";
+import OChristmasTree from "./songs/o_christmas_tree";
+import RingOut from "./songs/ring_out_wild_bells";
 import Rudolph from "./songs/rudolph_the_red_nosed_reindeer";
+import SomewhereIn from "./songs/somewhere_in_my_memory";
 import FirstNoel from "./songs/the_first_noel";
 import HouseTop from "./songs/up_on_the_house_top";
 import WishYou from "./songs/we_wish_you_a_merry_christmas";
+import WithWonderingAwe from "./songs/with_wondering_awe";
 
 export type NoteValue = "A" | "B" | "C" | "D" | "E" | "F" | "G";
 
@@ -41,19 +53,31 @@ export interface Library {
 const library: Library = {
   songs: [
     Angels as SongInfo,
+    AuldLangSyne as SongInfo,
     Away as SongInfo,
     ChristmasBells as SongInfo,
     Deck as SongInfo,
     FirstNoel as SongInfo,
+    GoodKing as SongInfo,
+    GoTellIt as SongInfo,
+    HappyBirthday as SongInfo,
+    Hark as SongInfo,
+    IHeardTheBells as SongInfo,
     Wonder as SongInfo,
+    JingleBells as SongInfo,
     Joy as SongInfo,
     Jolly as SongInfo,
     Drummer as SongInfo,
+    OhCome as SongInfo,
+    OChristmasTree as SongInfo,
     Sanctissima as SongInfo,
     OverRiver as SongInfo,
+    RingOut as SongInfo,
     Rudolph as SongInfo,
+    SomewhereIn as SongInfo,
     HouseTop as SongInfo,
     WishYou as SongInfo,
+    WithWonderingAwe as SongInfo,
   ],
 };
 
